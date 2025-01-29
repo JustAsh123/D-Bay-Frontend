044fe3a15413c29812f7bb695b4cd1e71cbe2de1
JustAsh123 <ashmit1268@gmail.com>
2025-01-30 00:45:11 +0530

    added different pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import Navbar from "./components/Navbar"
+import {Routes,Route} from 'react-router-dom'
 
 function App() {
   return (
     <div>
       <Navbar></Navbar>
+      <main>
+      <Routes>
+        <Route path="" element={<Home/>}></Route>
+        <Route path="/account-actions" element={<LoginSignup/>}></Route>
+      </Routes>
+      </main>
     </div>
   )
 }
